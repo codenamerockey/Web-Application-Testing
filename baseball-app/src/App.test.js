24cfea3f8884709374da5ad71584a1c8dd1322a7
Rockey Simmons <codenamerockey@gmail.com>
2019-09-05 15:57:23 -0700

    updated the test logic in the app.test

--- baseball-app/src/App.test.js
+++ baseball-app/src/App.test.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import Dashboard from './components/Dashboard';
 
-it('renders without crashing', () => {
+test('renders without crashing', () => {
   render(<App />);
 });
 
-test('Display component rendered', () => {
-  const container = render(<App />);
-  container.findByText(/STRIKES/);
+test('Dashboard Component rendered', () => {
+  render(<Dashboard />);
 });
+
+test('Display components rendered', () => {
+  const { findByText } = render(<App />);
+  findByText(/STRIKES/);
+  findByText(/BALLS/);
+  findByText(/FOULS/);
+  findByText(/HITS/);
+});
+
+// test('Display component rendered', () => {
+//   const { findByText } = render(<App />);
+// });
+
+// test('Display component rendered', () => {
+//   const { findByText } = render(<App />);
+// });
+
+// test('Display component rendered', () => {
+//   const container = render(<App />);
+// });
